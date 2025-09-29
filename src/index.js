f5c9b3c5a258a8dc82df7a7f51cbba66d4eaf699
Lu-gogo <XI-lank@outlook.com>
2025-09-29 12:19:19 +0800

    颜文字修改完成

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#52c41a', // 主色调改为绿色
+          colorSuccess: '#389e0d', // 成功色改为深绿色
+          colorInfo: '#52c41a',    // 信息色改为绿色
+          colorLink: '#52c41a',   // 链接色改为绿色
+          colorWarning: '#faad14', // 警告色保持不变
+          colorError: '#ff4d4f',   // 错误色保持不变
+        },
+      }}
+    ></ConfigProvider>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
